906f5042737710d650e1b4f876dfcaa4ef53684d
Hanno Braun <hanno@braun-embedded.com>
2021-03-15 15:04:49 +0100

    Execute step commands

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+mod num;
+
 use panic_rtt_target as _;
 
 #[rtic::app(device = lpc8xx_hal::pac, peripherals = false)]
@@ -11,8 +13,11 @@ mod app {
         Vec,
     };
     use lpc8xx_hal::{
-        nb,
+        delay::Delay,
+        gpio::{self, direction::Output, GpioPin},
+        mrt, nb,
         pac::USART0,
+        pins::{PIO0_16, PIO0_17, PIO0_18, PIO0_19, PIO0_20},
         prelude::*,
         usart::{
             self,
@@ -21,12 +26,21 @@ mod app {
     };
     use protocol::Command;
     use rtt_target::{rprint, rprintln};
+    use stepper::{
+        drivers::stspin220::STSPIN220, motion_control::SoftwareMotionControl,
+        ramp_maker, step_mode::StepMode256, Direction, Stepper,
+    };
+
+    use crate::num::{Num, SecondsToMrtTicks};
 
     #[resources]
     struct Resources {
         #[task_local]
         #[init(Queue(heapless::i::Queue::new()))]
         usart_queue: Queue<u8, U128>,
+
+        #[lock_free]
+        delay: Delay,
 
         #[lock_free]
         usart: usart::Rx<USART0, Enabled<u8, AsyncMode>>,
@@ -36,6 +50,23 @@ mod app {
 
         #[lock_free]
         usart_queue_cons: spsc::Consumer<'static, u8, U128>,
+
+        #[lock_free]
+        stepper: Stepper<
+            SoftwareMotionControl<
+                STSPIN220<
+                    (),
+                    GpioPin<PIO0_16, Output>,
+                    GpioPin<PIO0_17, Output>,
+                    GpioPin<PIO0_18, Output>,
+                    GpioPin<PIO0_19, Output>,
+                    GpioPin<PIO0_20, Output>,
+                >,
+                mrt::Channel<mrt::MRT0>,
+                ramp_maker::Trapezoidal<Num>,
+                SecondsToMrtTicks<mrt::MRT0>,
+            >,
+        >,
     }
 
     #[init(resources = [usart_queue])]
@@ -49,6 +80,8 @@ mod app {
         let mut syscon = p.SYSCON.split();
 
         let mut swm_handle = swm.handle.enable(&mut syscon.handle);
+
+        let delay = Delay::new(cx.core.SYST);
 
         let (u0_rxd, _) = swm
             .movable_functions
@@ -74,21 +107,66 @@ mod app {
         let (usart_queue_prod, usart_queue_cons) =
             cx.resources.usart_queue.split();
 
+        let gpio = p.GPIO.enable(&mut syscon.handle);
+        let mrt = p.MRT0.split(&mut syscon.handle);
+
+        let standby_reset = p
+            .pins
+            .pio0_16
+            .into_output_pin(gpio.tokens.pio0_16, gpio::Level::Low);
+        let mode1 = p
+            .pins
+            .pio0_17
+            .into_output_pin(gpio.tokens.pio0_17, gpio::Level::Low);
+        let mode2 = p
+            .pins
+            .pio0_18
+            .into_output_pin(gpio.tokens.pio0_18, gpio::Level::Low);
+        let step_mode3 = p
+            .pins
+            .pio0_19
+            .into_output_pin(gpio.tokens.pio0_19, gpio::Level::Low);
+        let dir_mode4 = p
+            .pins
+            .pio0_20
+            .into_output_pin(gpio.tokens.pio0_20, gpio::Level::Low);
+
+        let mut timer = mrt.mrt0;
+
+        let target_accel = Num::from_num(1000); // steps per second^2
+        let profile = ramp_maker::Trapezoidal::new(target_accel);
+
+        let stepper = Stepper::from_driver(STSPIN220::new())
+            .enable_step_control(step_mode3)
+            .enable_direction_control(dir_mode4, Direction::Forward, &mut timer)
+            .unwrap()
+            .enable_step_mode_control(
+                (standby_reset, mode1, mode2),
+                StepMode256::Full,
+                &mut timer,
+            )
+            .unwrap()
+            .enable_motion_control((timer, profile, SecondsToMrtTicks::new()));
+
         rprintln!("done.");
 
         (
             init::LateResources {
+                delay,
                 usart: usart.rx,
                 usart_queue_prod,
                 usart_queue_cons,
+                stepper,
             },
             init::Monotonics(),
         )
     }
 
-    #[idle(resources = [usart_queue_cons])]
+    #[idle(resources = [delay, usart_queue_cons, stepper])]
     fn idle(cx: idle::Context) -> ! {
+        let delay = cx.resources.delay;
         let usart = cx.resources.usart_queue_cons;
+        let stepper = cx.resources.stepper;
 
         let mut buf: Vec<_, U128> = Vec::new();
 
@@ -101,7 +179,54 @@ mod app {
                         postcard::from_bytes_cobs(&mut buf).unwrap();
                     buf.clear();
 
-                    rprintln!("{:?}", command);
+                    rprintln!("Executing command: {:?}", command);
+
+                    match command {
+                        Command::Step(step) => {
+                            // There are two sources for panics in the
+                            // following code:
+                            // - Ongoing motion: The driver only provides
+                            //   support for `set_direction` and `step`, if no
+                            //   motion is currently ongoing.
+                            // - Errors from the `OutputPin` or
+                            //   `timer::CountDown` implementations.
+                            //
+                            // Since we currently don't use the motion
+                            // control API, and our implementations of those
+                            // traits are infallible, neither of those can
+                            // actually happen.
+
+                            match step.direction {
+                                protocol::Direction::Forward => {
+                                    stepper
+                                        .driver_mut()
+                                        .set_direction(Direction::Forward)
+                                        .expect("Ongoing motion")
+                                        .wait()
+                                        .expect("Pin or timer error");
+                                }
+                                protocol::Direction::Backward => {
+                                    stepper
+                                        .driver_mut()
+                                        .set_direction(Direction::Backward)
+                                        .expect("Ongoing motion")
+                                        .wait()
+                                        .expect("Pin or timer error");
+                                }
+                            }
+
+                            for _ in 0..step.steps {
+                                stepper
+                                    .driver_mut()
+                                    .step()
+                                    .expect("Ongoing motion")
+                                    .wait()
+                                    .expect("Pin or timer error");
+
+                                delay.delay_ms(step.delay);
+                            }
+                        }
+                    }
                 }
             }
         }
---
+++ firmware/src/num.rs
@@ -0,0 +1,53 @@
+use core::{
+    convert::{Infallible, TryFrom as _},
+    marker::PhantomData,
+};
+
+use lpc8xx_hal::mrt;
+use num_traits::Zero as _;
+use stepper::{
+    embedded_hal::timer,
+    embedded_time::duration::{
+        Microseconds, Milliseconds, Nanoseconds, Seconds,
+    },
+    motion_control::DelayToTicks,
+};
+
+pub type Num = fixed::FixedI64<typenum::U32>;
+
+pub struct SecondsToMrtTicks<I>(PhantomData<I>);
+
+impl<I> SecondsToMrtTicks<I> {
+    pub fn new() -> Self {
+        Self(PhantomData)
+    }
+}
+
+impl<I> DelayToTicks<Num> for SecondsToMrtTicks<I>
+where
+    I: mrt::Trait,
+{
+    type Ticks = <mrt::Channel<I> as timer::CountDown>::Time;
+
+    type Error = Infallible;
+
+    fn delay_to_ticks(&self, delay: Num) -> Result<Self::Ticks, Self::Error> {
+        let mut ticks = mrt::Ticks::zero();
+
+        let delay_s = delay;
+        ticks += mrt::Ticks::try_from(Seconds(delay_s.int().to_num())).unwrap();
+
+        let delay_ms = delay_s.frac() * 1000;
+        ticks += mrt::Ticks::try_from(Milliseconds(delay_ms.int().to_num()))
+            .unwrap();
+
+        let delay_us = delay_ms.frac() * 1000;
+        ticks += mrt::Ticks::try_from(Microseconds(delay_us.int().to_num()))
+            .unwrap();
+
+        let delay_ns = delay_us.frac() * 1000;
+        ticks += mrt::Ticks::from(Nanoseconds(delay_ns.int().to_num()));
+
+        Ok(ticks)
+    }
+}
